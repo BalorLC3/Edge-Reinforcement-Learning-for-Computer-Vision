f47c51e6ef686ea6d9005320095ce130fde5487c
Balor.LC3 <supernova20315@gmail.com>
2026-02-25 11:13:32 -0700

    New stage

--- include/edgecv/core.hpp
+++ include/edgecv/core.hpp
@@ -8,13 +8,18 @@ namespace edgecv {
         Point2f () : x(0), y(0) {}
         Point2f(float _x, float _y) : x(_x), y(_y) {}
     };
+
     // Lightweight 3x3 Matrix for 2D transformation
+
     struct Mat3x3 {
         double data[9] = {1, 0, 0, 0, 1, 0, 0, 0, 1}; // Identity default
-
+        
         double& at(int row, int col) {return data [row * 3 + col]; }
         const double& at (int row, int col) const { return data[row * 3 + col]; }
     };
 
-    enum class MotionModel { TRANSLATION, AFFINE, PERSPECTIVE };
-} // namespace edgecv
+    enum class MotionModel { TRANSLATION, AFFINE, PERSPECTIVE, RIGID, SIMILARITY };
+
+    
+
+} // namespace
